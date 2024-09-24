43d9cdfed2053b24507a918490a25735634abc08
EgorSivenko <egor.siv20@gmail.com>
2024-09-24 13:08:24 +0300

    Converted `UserMapper` to the method of `RegisterRequest`

--- src/main/java/org/acme/auth/request/RegisterRequest.java
+++ src/main/java/org/acme/auth/request/RegisterRequest.java
@@ -1,8 +1,14 @@
 package org.acme.auth.request;
 
+import org.acme.user.User;
+
 public record RegisterRequest(
         String fullName,
         String email,
         String password,
         String recaptchaToken
-) {}
+) {
+    public User mapToUser() {
+        return new User(fullName, email, password);
+    }
+}
