edb5e5f4d32748d480d85d0aea3455712baa4d5b
EgorSivenko <egor.siv20@gmail.com>
2024-10-11 23:47:15 +0300

    Revert email sending to use MailTemplateInstance

--- src/main/java/org/acme/email/SimpleEmailSender.java
+++ src/main/java/org/acme/email/SimpleEmailSender.java
@@ -1,9 +1,7 @@
 package org.acme.email;
 
-import io.quarkus.mailer.Mail;
-import io.quarkus.mailer.Mailer;
+import io.quarkus.mailer.MailTemplate.MailTemplateInstance;
 import io.quarkus.qute.CheckedTemplate;
-import io.quarkus.qute.TemplateInstance;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.UriBuilder;
@@ -21,42 +19,41 @@ public class SimpleEmailSender implements EmailSender {
             throw new IllegalStateException("Utility class");
         }
 
-        public static native TemplateInstance registrationEmail(String link);
-        public static native TemplateInstance registrationEmailPlain(String link);
+        public static native MailTemplateInstance registrationEmail(String link);
 
-        public static native TemplateInstance resetPasswordEmail(String link);
-        public static native TemplateInstance resetPasswordEmailPlain(String link);
+        public static native MailTemplateInstance resetPasswordEmail(String link);
     }
+
+    private static final String REGISTRATION_SUBJECT = "Confirm your email";
+    private static final String RESET_PASSWORD_SUBJECT = "Confirm password reset";
 
     @Context
     UriInfo uriInfo;
 
     private final TokenService tokenService;
-    private final Mailer mailer;
 
-    public SimpleEmailSender(TokenService tokenService, Mailer mailer) {
+    public SimpleEmailSender(TokenService tokenService) {
         this.tokenService = tokenService;
-        this.mailer = mailer;
     }
 
     @Override
     public void sendRegistrationEmail(User recipient) {
         String link = formatLink(recipient, "registration");
 
-        String htmlContent = Templates.registrationEmail(link).render();
-        String plainTextContent = Templates.registrationEmailPlain(link).render();
-
-        sendMultipartEmail(recipient.getEmail(), "Confirm your email", htmlContent, plainTextContent);
+        Templates.registrationEmail(link)
+                .to(recipient.getEmail())
+                .subject(REGISTRATION_SUBJECT)
+                .send().await().indefinitely();
     }
 
     @Override
     public void sendResetPasswordEmail(User recipient) {
         String link = formatLink(recipient, "reset-password");
 
-        String htmlContent = Templates.resetPasswordEmail(link).render();
-        String plainTextContent = Templates.resetPasswordEmailPlain(link).render();
-
-        sendMultipartEmail(recipient.getEmail(), "Reset password", htmlContent, plainTextContent);
+        Templates.resetPasswordEmail(link)
+                .to(recipient.getEmail())
+                .subject(RESET_PASSWORD_SUBJECT)
+                .send().await().indefinitely();
     }
 
     private String formatLink(User recipient, String linkPart) {
@@ -67,13 +64,5 @@ public class SimpleEmailSender implements EmailSender {
                 .queryParam("token", token)
                 .build()
                 .toString();
-    }
-
-    private void sendMultipartEmail(String to, String subject,
-                                    String htmlContent, String plainTextContent) {
-        mailer.send(
-                Mail.withHtml(to, subject, htmlContent)
-                        .setText(plainTextContent)
-        );
     }
 }
