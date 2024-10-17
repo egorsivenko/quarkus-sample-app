5adac922f0a6d28c20c601325e06edc003cae2bb
EgorSivenko <egor.siv20@gmail.com>
2024-10-17 23:02:57 +0300

    Set up and configure Hibernate ORM with Panache

--- src/main/java/org/acme/user/UserService.java
+++ src/main/java/org/acme/user/UserService.java
@@ -2,65 +2,68 @@ package org.acme.user;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
+import jakarta.transaction.Transactional;
 import org.acme.admin.form.EditUserForm;
 import org.acme.user.exception.EmailAlreadyTakenException;
 import org.acme.user.exception.UserNotFoundException;
-import org.acme.user.repository.UserRepository;
+import org.acme.user.repository.PanacheUserRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Named("userService")
 @ApplicationScoped
+@Transactional
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final PanacheUserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(PanacheUserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public List<User> listAll() {
-        return userRepository.findAll();
+        return userRepository.listAll();
     }
 
     public User getById(UUID id) {
-        return userRepository.findById(id)
+        return userRepository.findByIdOptional(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
     public User getByEmail(String email) {
-        return userRepository.findByEmail(email)
+        return userRepository.findByEmailOptional(email)
                 .orElseThrow(() -> new UserNotFoundException(email));
     }
 
     public boolean existsByEmail(String email) {
-        return userRepository.existsByEmail(email);
+        return userRepository.findByEmailOptional(email)
+                .isPresent();
     }
 
     public void create(User user) {
-        if (userRepository.existsByEmail(user.getEmail())) {
+        if (existsByEmail(user.getEmail())) {
             throw new EmailAlreadyTakenException(user.getEmail());
         }
-        userRepository.save(user);
+        userRepository.persist(user);
     }
 
     public void edit(EditUserForm form) {
         User user = getById(form.getId());
         String newEmail = form.getEmail();
 
-        if (!user.getEmail().equals(newEmail) && userRepository.existsByEmail(newEmail)) {
+        if (!user.getEmail().equals(newEmail) && existsByEmail(newEmail)) {
             throw new EmailAlreadyTakenException(newEmail);
         }
         user.setEmail(newEmail);
         user.setFullName(form.getFullName());
         user.setRole(UserRole.valueOf(form.getRole().toUpperCase()));
 
-        userRepository.update(form.getId(), user);
+        userRepository.persist(user);
     }
 
     public void delete(UUID id) {
-        userRepository.delete(id);
+        userRepository.deleteById(id);
     }
 
     public boolean isUserAdmin(User user) {
