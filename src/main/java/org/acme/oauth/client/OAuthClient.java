03bf0ebffdb48764da5d9aa8d436e13bbac9e2b3
EgorSivenko <egor.siv20@gmail.com>
2024-11-13 12:12:27 +0200

    Add one-to-many mapping between `User` and `OAuthClient`

--- src/main/java/org/acme/oauth/client/OAuthClient.java
+++ src/main/java/org/acme/oauth/client/OAuthClient.java
@@ -6,7 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import org.acme.user.User;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.UUID;
 
@@ -32,4 +37,9 @@ public class OAuthClient extends PanacheEntityBase {
 
     @Column(name = "callback_url", nullable = false)
     public String callbackUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    public User user;
 }
