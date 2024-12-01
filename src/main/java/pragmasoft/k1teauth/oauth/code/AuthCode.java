f444c3ccdbbce2764d35d0e98ab42e4a7352762a
EgorSivenko <egor.siv20@gmail.com>
2024-12-01 23:04:28 +0200

    Remove a redundant `id` field of type `UUID` in AuthCode

--- src/main/java/pragmasoft/k1teauth/oauth/code/AuthCode.java
+++ src/main/java/pragmasoft/k1teauth/oauth/code/AuthCode.java
@@ -3,28 +3,22 @@ package pragmasoft.k1teauth.oauth.code;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import pragmasoft.k1teauth.oauth.client.OAuthClient;
-import pragmasoft.k1teauth.user.User;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import pragmasoft.k1teauth.oauth.client.OAuthClient;
+import pragmasoft.k1teauth.user.User;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Entity
 @Table(name = "auth_codes")
 public class AuthCode extends PanacheEntityBase {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    public UUID id;
-
     @Column(nullable = false, unique = true)
     public String code;
 
