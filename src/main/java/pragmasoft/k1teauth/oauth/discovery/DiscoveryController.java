0c0b0707bbd4391237a241676500ccd2986f38cd
EgorSivenko <egor.siv20@gmail.com>
2025-02-13 01:27:49 +0200

    Update Authorization Server Metadata

--- src/main/java/pragmasoft/k1teauth/oauth/discovery/DiscoveryController.java
+++ src/main/java/pragmasoft/k1teauth/oauth/discovery/DiscoveryController.java
@@ -1,4 +1,4 @@
-package pragmasoft.k1teauth.oidc.discovery;
+package pragmasoft.k1teauth.oauth.discovery;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
@@ -7,31 +7,33 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import pragmasoft.k1teauth.common.ServerInfo;
-import pragmasoft.k1teauth.oauth.util.CodeChallengeUtil;
 import pragmasoft.k1teauth.oauth.TokenRequestHandler;
+import pragmasoft.k1teauth.oauth.util.CodeChallengeUtil;
 
 import java.util.Set;
 
 @Controller("/.well-known")
 @Secured(SecurityRule.IS_ANONYMOUS)
-public class OidcDiscoveryController {
+public class DiscoveryController {
 
     private final ServerInfo serverInfo;
 
-    public OidcDiscoveryController(ServerInfo serverInfo) {
+    public DiscoveryController(ServerInfo serverInfo) {
         this.serverInfo = serverInfo;
     }
 
     @Get(uri = "/openid-configuration", produces = MediaType.APPLICATION_JSON)
     public HttpResponse<?> oidcDiscovery() {
-        OidcMetadata metadata = new OidcMetadata();
+        Metadata metadata = new Metadata();
         String baseUrl = serverInfo.getBaseUrl();
 
         metadata.setIssuer(baseUrl);
         metadata.setAuthorizationEndpoint(baseUrl + "/oauth2/auth");
         metadata.setTokenEndpoint(baseUrl + "/oauth2/token");
         metadata.setTokenEndpointAuthMethodsSupported(Set.of("client_secret_basic"));
+        metadata.setUserInfoEndpoint(baseUrl + "/oauth2/userinfo");
 
+        metadata.setScopesSupported(Set.of("openid"));
         metadata.setResponseTypesSupported(Set.of("code"));
         metadata.setResponseModesSupported(Set.of("query"));
 
