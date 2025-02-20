164187947850cf88f0fdbd9261fdecc8248824bf
EgorSivenko <egor.siv20@gmail.com>
2025-02-20 02:07:09 +0200

    Remove Startup Event Listener

--- src/main/java/pragmasoft/k1teauth/Application.java
+++ src/main/java/pragmasoft/k1teauth/Application.java
@@ -1,16 +1,11 @@
 package pragmasoft.k1teauth;
 
-import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.runtime.event.annotation.EventListener;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import jakarta.inject.Singleton;
-import pragmasoft.k1teauth.common.ServerInfo;
-import pragmasoft.k1teauth.oauth.scope.Scope;
-import pragmasoft.k1teauth.oauth.scope.ScopeRepository;
 
 @Singleton
 @OpenAPIDefinition(
@@ -27,23 +22,7 @@ import pragmasoft.k1teauth.oauth.scope.ScopeRepository;
 )
 public class Application {
 
-    private final ScopeRepository scopeRepository;
-    private final ServerInfo serverInfo;
-
-    public Application(ScopeRepository scopeRepository,
-                       ServerInfo serverInfo) {
-        this.scopeRepository = scopeRepository;
-        this.serverInfo = serverInfo;
-    }
-
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
-    }
-
-    @EventListener
-    public void onStartup(StartupEvent startupEvent) {
-        if (!scopeRepository.existsById("openid")) {
-            scopeRepository.save(new Scope("openid", "Obtain basic profile information", serverInfo.getBaseUrl()));
-        }
     }
 }
