2ee321dd873480b73fa0be565a52591e9f5eca1f
EgorSivenko <egor.siv20@gmail.com>
2025-03-03 16:14:01 +0200

    Implement Hotwire Turbo for `AdminController`

--- src/main/java/pragmasoft/k1teauth/admin/AdminController.java
+++ src/main/java/pragmasoft/k1teauth/admin/AdminController.java
@@ -8,18 +8,16 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Error;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.security.annotation.Secured;
-import io.micronaut.views.ModelAndView;
-import io.micronaut.views.View;
-import io.micronaut.views.fields.Form;
 import io.micronaut.views.fields.FormGenerator;
 import io.micronaut.views.fields.messages.Message;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import pragmasoft.k1teauth.admin.form.EditUserForm;
-import pragmasoft.k1teauth.user.User;
+import pragmasoft.k1teauth.common.jte.JteTemplateRenderer;
 import pragmasoft.k1teauth.user.UserService;
 import pragmasoft.k1teauth.user.exception.EmailAlreadyTakenException;
 
@@ -34,54 +32,59 @@ import java.util.UUID;
 @Secured("ADMIN")
 public class AdminController {
 
-    protected static final String EDIT_USER_PATH = "/admin/edit-user";
+    private static final String USERS_LIST_PATH = "/admin/users-list";
+    private static final String EDIT_USER_PATH = "/admin/edit-user";
+    private static final String USERS_LIST_TEMPLATE = "admin/usersList.jte";
+    private static final String EDIT_USER_TEMPLATE = "admin/editUser.jte";
 
     private final UserService userService;
     private final FormGenerator formGenerator;
+    private final JteTemplateRenderer jteTemplateRenderer;
 
     public AdminController(UserService userService,
-                           FormGenerator formGenerator) {
+                           FormGenerator formGenerator,
+                           JteTemplateRenderer jteTemplateRenderer) {
         this.userService = userService;
         this.formGenerator = formGenerator;
+        this.jteTemplateRenderer = jteTemplateRenderer;
     }
 
-    @View("admin/usersList")
     @Get(uri = "/users-list", produces = MediaType.TEXT_HTML)
-    public Map<String, Object> usersList() {
-        return Map.of("users", userService.listAll(), "formGenerator", formGenerator);
+    public String usersList() {
+        return jteTemplateRenderer.render(USERS_LIST_TEMPLATE,
+                Map.of("users", userService.listAll(), "formGenerator", formGenerator));
     }
 
-    @View("admin/editUser")
     @Get(uri = "/edit-user", produces = MediaType.TEXT_HTML)
-    public Map<String, Form> editUserTemplate(@QueryValue UUID id) {
-        User user = userService.getById(id);
-        return Map.of("form", formGenerator.generate(EDIT_USER_PATH, EditUserForm.from(user)));
+    public String editUserTemplate(@QueryValue UUID id) {
+        return jteTemplateRenderer.render(EDIT_USER_TEMPLATE,
+                Map.of("form", formGenerator.generate(EDIT_USER_PATH, EditUserForm.from(userService.getById(id)))));
     }
 
-    @Post(uri = "/edit-user", consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    public HttpResponse<?> editUser(@Valid @Body EditUserForm form) {
+    @Post(uri = "/edit-user", consumes = MediaType.APPLICATION_FORM_URLENCODED, produces = MediaType.TEXT_HTML)
+    public HttpResponse<String> editUser(@Valid @Body EditUserForm form) {
         try {
             userService.edit(form);
-            return HttpResponse.seeOther(URI.create("/admin/users-list"));
+            return HttpResponse.seeOther(URI.create(USERS_LIST_PATH));
         } catch (EmailAlreadyTakenException e) {
-            return HttpResponse.badRequest(new ModelAndView<>("admin/editUser",
+            return HttpResponse.badRequest(jteTemplateRenderer.render(EDIT_USER_TEMPLATE,
                     Map.of("form", formGenerator.generate(EDIT_USER_PATH, form),
-                            "errors", List.of(Message.of("Email address is already registered")))
-            ));
+                            "errors", List.of(Message.of("Email address is already registered")))));
         }
     }
 
-    @Post(uri = "/delete-user", consumes = MediaType.APPLICATION_FORM_URLENCODED)
+    @Post(uri = "/delete-user", consumes = MediaType.APPLICATION_FORM_URLENCODED, produces = MediaType.TEXT_HTML)
     public HttpResponse<Void> deleteUser(UUID id) {
         userService.delete(id);
-        return HttpResponse.seeOther(URI.create("/admin/users-list"));
+        return HttpResponse.seeOther(URI.create(USERS_LIST_PATH));
     }
 
     @Error(exception = ConstraintViolationException.class)
-    public HttpResponse<?> handleError(HttpRequest<EditUserForm> request, ConstraintViolationException ex) {
+    @Produces(MediaType.TEXT_HTML)
+    public HttpResponse<String> handleError(HttpRequest<EditUserForm> request, ConstraintViolationException ex) {
         Optional<EditUserForm> formOptional = request.getBody();
         return formOptional.isPresent()
-                ? HttpResponse.unprocessableEntity().body(new ModelAndView<>("admin/editUser",
+                ? HttpResponse.unprocessableEntity().body(jteTemplateRenderer.render(EDIT_USER_TEMPLATE,
                         Map.of("form", formGenerator.generate(EDIT_USER_PATH, formOptional.get(), ex))))
                 : HttpResponse.serverError();
     }
