a6325e949807990fe770ca93403d58a1d97633a7
EgorSivenko <egor.siv20@gmail.com>
2025-03-05 12:32:54 +0200

    Implement Fieldset Generation and Hotwire Turbo for `LoginController`

--- src/main/java/pragmasoft/k1teauth/auth/LoginController.java
+++ src/main/java/pragmasoft/k1teauth/auth/LoginController.java
@@ -1,15 +1,17 @@
 package pragmasoft.k1teauth.auth;
 
 import io.micronaut.context.annotation.Property;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
-import io.micronaut.views.View;
+import io.micronaut.views.fields.FormGenerator;
+import io.micronaut.views.fields.messages.Message;
 import io.swagger.v3.oas.annotations.Hidden;
+import pragmasoft.k1teauth.auth.form.LoginForm;
+import pragmasoft.k1teauth.common.jte.JteTemplateRenderer;
 
 import java.util.Map;
 
@@ -18,12 +20,25 @@ import java.util.Map;
 @Secured(SecurityRule.IS_ANONYMOUS)
 public class LoginController {
 
+    private static final String LOGIN_PATH = "/login";
+    private static final String LOGIN_TEMPLATE = "auth/login.jte";
+
     @Property(name = "turnstile.siteKey")
     private String siteKey;
 
-    @View("auth/login")
+    private final FormGenerator formGenerator;
+    private final JteTemplateRenderer jteTemplateRenderer;
+
+    public LoginController(FormGenerator formGenerator,
+                           JteTemplateRenderer jteTemplateRenderer) {
+        this.formGenerator = formGenerator;
+        this.jteTemplateRenderer = jteTemplateRenderer;
+    }
+
     @Get(uri = "/login", produces = MediaType.TEXT_HTML)
-    public HttpResponse<?> login(@QueryValue(defaultValue = "false") boolean failed) {
-        return HttpResponse.ok(Map.of("siteKey", siteKey, "failed", failed));
+    public String login(@QueryValue(defaultValue = "false") boolean failed) {
+        return jteTemplateRenderer.render(LOGIN_TEMPLATE,
+                Map.of("form", formGenerator.generate(LOGIN_PATH, LoginForm.class, Message.of("Login")),
+                        "siteKey", siteKey, "failed", failed));
     }
 }
