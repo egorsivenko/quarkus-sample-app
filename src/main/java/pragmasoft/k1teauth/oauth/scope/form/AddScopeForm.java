c7921ce9f980c495449e525e194a2a0945551318
Dmytro Zemnytskyi <pragmasoft@gmail.com>
2025-03-06 21:08:15 +0200

    minor fixes - warning, VSCode specific classpath error

--- src/main/java/pragmasoft/k1teauth/oauth/scope/form/AddScopeForm.java
+++ src/main/java/pragmasoft/k1teauth/oauth/scope/form/AddScopeForm.java
@@ -7,58 +7,56 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import pragmasoft.k1teauth.oauth.scope.Scope;
 
-import java.util.Objects;
-
 @Serdeable
 public class AddScopeForm {
 
-    @NotBlank
-    @Size(min = 3, max = 255)
-    protected String name;
+  @NotBlank
+  @Size(min = 3, max = 255)
+  protected String name;
 
-    @NotBlank
-    @Size(min = 3, max = 255)
-    protected String description;
+  @NotBlank
+  @Size(min = 3, max = 255)
+  protected String description;
 
-    @NotBlank
-    @InputUrl
-    @Size(max = 255)
-    protected String audience;
+  @NotBlank
+  @InputUrl
+  @Size(max = 255)
+  protected String audience;
 
-    public AddScopeForm() {}
+  public AddScopeForm() {}
 
-    @Creator
-    public AddScopeForm(String name, String description, String audience) {
-        this.name = name;
-        this.description = description;
-        this.audience = audience;
-    }
+  @Creator
+  public AddScopeForm(String name, String description, String audience) {
+    this.name = name;
+    this.description = description;
+    this.audience = audience;
+  }
 
-    public Scope mapToScope() {
-        return new Scope(name, description, audience);
-    }
+  public Scope mapToScope() {
+    return new Scope(name, description, audience);
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public String getAudience() {
-        return audience;
-    }
+  public String getAudience() {
+    return audience;
+  }
 
-    public void setAudience(String audience) {
-        this.audience = audience;
-    }
+  public void setAudience(String audience) {
+    this.audience = audience;
+  }
 }
