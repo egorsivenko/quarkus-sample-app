a9a1e996d36acebb486315cb7ec51883a28843da
EgorSivenko <egor.siv20@gmail.com>
2025-03-07 21:17:23 +0200

    Implement Fieldset Generation and Hotwire Turbo for `ConsentController`

--- src/main/java/pragmasoft/k1teauth/oauth/consent/ConsentController.java
+++ src/main/java/pragmasoft/k1teauth/oauth/consent/ConsentController.java
@@ -7,9 +7,9 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
-import io.micronaut.views.View;
+import io.micronaut.views.fields.FormGenerator;
 import io.swagger.v3.oas.annotations.Hidden;
-import jakarta.transaction.Transactional;
+import pragmasoft.k1teauth.common.jte.JteTemplateRenderer;
 import pragmasoft.k1teauth.user.User;
 import pragmasoft.k1teauth.user.UserService;
 
@@ -23,26 +23,34 @@ import java.util.UUID;
 @Secured(SecurityRule.IS_AUTHENTICATED)
 public class ConsentController {
 
+    private static final String CONSENTS_PATH = "/granted-consents";
+    private static final String CONSENTS_TEMPLATE = "oauth/grantedConsents.jte";
+
     private final UserService userService;
     private final ConsentRepository consentRepository;
+    private final FormGenerator formGenerator;
+    private final JteTemplateRenderer jteTemplateRenderer;
 
     public ConsentController(UserService userService,
-                             ConsentRepository consentRepository) {
+                             ConsentRepository consentRepository,
+                             FormGenerator formGenerator,
+                             JteTemplateRenderer jteTemplateRenderer) {
         this.userService = userService;
         this.consentRepository = consentRepository;
+        this.formGenerator = formGenerator;
+        this.jteTemplateRenderer = jteTemplateRenderer;
     }
 
-    @View("oauth/grantedConsents")
     @Get(produces = MediaType.TEXT_HTML)
-    public HttpResponse<?> grantedConsents(Principal principal) {
+    public String grantedConsents(Principal principal) {
         User user = userService.getByEmail(principal.getName());
-        return HttpResponse.ok(Map.of("consents", consentRepository.findAllByResourceOwner(user)));
+        return jteTemplateRenderer.render(CONSENTS_TEMPLATE,
+                Map.of("consents", consentRepository.findAllByResourceOwner(user), "formGenerator", formGenerator));
     }
 
-    @Post(uri = "/revoke", consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    @Transactional
-    public HttpResponse<?> revokeConsent(UUID id) {
+    @Post(uri = "/revoke", consumes = MediaType.APPLICATION_FORM_URLENCODED, produces = MediaType.TEXT_HTML)
+    public HttpResponse<Void> revokeConsent(UUID id) {
         consentRepository.deleteById(id);
-        return HttpResponse.redirect(URI.create("/granted-consents"));
+        return HttpResponse.seeOther(URI.create(CONSENTS_PATH));
     }
 }
